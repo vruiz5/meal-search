061a0d3c34ecc53dfd184a6c7cf424cfdcd4d5cb
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-05-05 22:37:43 -0500

    adding dist

---
+++ assets/OtherPage-BRjti3Kt.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s,a as c}from"./index-4SRQ3dz5.js";const n={},o={class:"flex min-h-screen items-center justify-center"},a=c("h1",{class:"text-6xl font-thin text-slate-800"},"I'm Other Page",-1),r=[a];function _(i,f){return s(),t("main",o,r)}const l=e(n,[["render",_]]);export{l as default};
